82e6d1e320859601a535dfc92b08ecb19394cae0
thanhtruongnhu <35893692+thanhtruongnhu@users.noreply.github.com>
2022-08-31 17:29:15 -0300

    add helpers

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ var https = require('https');
 const { type } = require('os');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 
 // Instantiate the HTTP server 
@@ -76,7 +78,7 @@ var unifiedServer = function(req,res){
         'queryStringObject' : queryStringObject,
         'method' : method,
         'headers' : headers,
-        'payload' : buffer
+        'payload' : helpers.parseJsonToObject(buffer)
     };
 
     // Router the request to the handler specified in the router
@@ -102,20 +104,8 @@ var unifiedServer = function(req,res){
 };
 
 
-//  Define handlers
-var handlers = {}; //object
-
-// Ping handler
-handlers.ping = function(data,callback){
-    callback(200);
-};
-
-// not found handler
-handlers.notFound = function(data,callback){
-    callback(404);
-};
-
 // Define a request router
 var router = {
-    'ping' : handlers.ping  
+    'ping' : handlers.ping  ,
+    'users': handlers.users
 };
